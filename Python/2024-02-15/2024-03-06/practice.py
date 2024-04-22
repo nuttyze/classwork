bfc98f0fc17e1746b7e9bf7243377ae88f6a7756
Christopher <chernandez35@bostonk12.org>
2024-04-22 09:56:06 -0400

    Python Folder

---
+++ Python/2024-02-15/2024-03-06/practice.py
@@ -0,0 +1,11 @@
+#Chris
+# 2024-03-06
+
+a = input("Please enter your name:")
+b =","
+print('Hi',a + b , "I hope you are having an awesome day!")
+
+G =input('please tell me your gender:')
+p =input('your nouns are:')
+print(f'your gender is {G}, and your nouns are: {p}')
+
