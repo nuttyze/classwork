ff68d09e1e653956f496aa2b23af2e739c4e14a1
Christopher <chernandez35@bostonk12.org>
2024-04-25 10:36:45 -0400

    Python Folder

---
+++ Python/Week_16/practice3.py
@@ -0,0 +1,17 @@
+# christopher hernandez
+# 2024-04-25
+
+# This is how you create an empty list
+a = []
+
+# This i how to create a list with values
+b = [ 1,2,3]
+
+print(a)
+print(b)
+
+a+=['chris']
+b+=[4]
+
+print(a)
+print(b)
