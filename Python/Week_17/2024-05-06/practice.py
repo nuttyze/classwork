5b6d646af08e657698e60516333989fd466d565b
Christopher <chernandez35@bostonk12.org>
2024-05-06 10:42:41 -0400

    Python Folder

---
+++ Python/Week_17/2024-05-06/practice.py
@@ -0,0 +1,22 @@
+# Christopher hernandez
+# 2024-05-06
+
+numbers = []
+numbers += [1]
+numbers += [2]
+numbers += [3]
+numbers += [4]
+numbers += [5]
+numbers += [6]
+numbers += [7]
+numbers += [8]
+numbers += [9]
+numbers += [10]
+print(numbers)
+numbers2 = []
+
+for b in range(1,1001):
+    numbers2.append(b)
+
+print(numbers)
+print(numbers2)
