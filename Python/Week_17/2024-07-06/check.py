575c7d9f565ef24107a27f6694cc9050cf22e082
Christopher <chernandez35@bostonk12.org>
2024-05-07 10:30:32 -0400

    Python Folder

---
+++ Python/Week_17/2024-07-06/check.py
@@ -0,0 +1,30 @@
+# christopher hernandez
+# 2024-07-06
+
+# creating an empty list
+a = []
+
+# using the operator to add the list
+a += [1]
+a += [2]
+a += [3]
+
+# printing the list
+print(a)
+
+# creating an empty list named b
+b = []
+
+# using the function to add the list b
+b.append(1)
+b.append(2)
+b.append(3)
+
+# printing the list
+print(b)
+
+# posistional comperison
+print(a==b)
+
+# object comperison
+print(a is b)
