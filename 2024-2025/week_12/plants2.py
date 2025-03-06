dd0cee60ab318fbdacae883f241dd80523f4c5c1
chris <chernadez35@bostonk12.org>
2025-03-06 14:06:13 -0500

    voc

---
+++ 2024-2025/week_12/plants2.py
@@ -0,0 +1,7 @@
+def main():
+   plant1 = input("Please pick a number: ")
+   plant2  = input("pick a number: ")
+
+   print(f"{plant1} + {plant2} = {plant1 + plant2}")
+if __name__ == '__main__':
+    main()
